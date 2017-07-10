4601d37826f0d22e738a5859eacccf340a4491fe
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-07-10 17:19:58 -0400

    Add event listeners for keyboard input and the reset button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const testWrapper = document.querySelector(".test-wrapper");
-const testArea = document.querySelector("#test-area");
-const originText = document.querySelector("#origin-text p").innerHTML;
-const resetButton = document.querySelector("#reset");
-const theTimer = document.querySelector(".timer");
+var testWrapper = document.querySelector(".test-wrapper");
+var testArea = document.querySelector("#test-area");
+var originText = document.querySelector("#origin-text p").innerHTML;
+var resetButton = document.querySelector("#reset");
+var theTimer = document.querySelector(".timer");
 
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
@@ -12,12 +12,23 @@ const theTimer = document.querySelector(".timer");
 
 
 // Match the text entered with the provided text on the page:
-
+function spellCheck() {
+	var textEntered = testArea.value;
+	console.log(textEntered);
+}
 
 // Start the timer:
-
+function start() {
+	var textEnteredLength = testArea.value.length;
+	console.log(textEnteredLength);
+}
 
 // Reset everything:
-
+function reset() {
+	console.log("reset button has been pressed!");
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
